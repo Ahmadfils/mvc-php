1b256778e1a4534cff8afa6d60bac02181256c23
Abaone <ahmadfils257@gmail.com>
2025-05-04 23:48:31 +0200

    dynamical news

--- core/Router.php
+++ core/Router.php
@@ -72,15 +72,4 @@ class Router
     }
 }
 
-class Request
-{
-    public function getPath()
-    {
-        $path = $_SERVER['REQUEST_URI'] ?? '/';
-        $position = strpos($path, '?');
-        if ($position === false) {
-            return $path;
-        }
-        return substr($path, 0, $position);
-    }
-}
+
